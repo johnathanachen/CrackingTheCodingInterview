2de7392b308d68e1f943f691ec312993664b7b39
elmerastudillo <astudilloelmer@icloud.com>
2017-11-27 21:34:02 -0800

    Implemented Dynamic array in swift

--- CrackingTheCodingInterview.playground/Sources/Array&Strings.swift
+++ CrackingTheCodingInterview.playground/Sources/Array&Strings.swift
@@ -2,6 +2,24 @@ import Foundation
 
 // ArrayList & Resizable Arrays
 
+// MARK: - Dynamic Array
+// Demonstrating the use case of a Dynamic array
+// A dynamic array resizes itself * 2 everytime the load factor is close to being hit
+func arrayList(words: [String], more: [String]) -> [String]
+{
+    var sentence = [String]()
+    for w in words
+    {
+        sentence.append(w)
+    }
+    
+    for m in more
+    {
+        sentence.append(m)
+    }
+    
+    return sentence
+}
 
 // StringBuilder
 
